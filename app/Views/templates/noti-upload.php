db3efced0032b52b2deb7853cc97ce096cc0c4e7
Afif Dev <afif_882003@yahoo.com>
2022-07-02 13:38:07 +0800

    Initial commit

---
+++ app/Views/templates/noti-upload.php
@@ -0,0 +1,4 @@
+<div class="notification is-warning">
+  <strong>Warning - </strong> application allow temporary file upload. Which mean file will be remove once it's uploaded into the application.
+  Check <a href="/documentation">docs</a> to fully enable the file upload.
+</div>
